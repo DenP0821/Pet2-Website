eda8caead2f09c5e6b64e050149889c204b3baa9
Denis <160841197+DenP0821@users.noreply.github.com>
2025-06-29 02:06:51 +0300

    День 5

---
+++ script.js
@@ -0,0 +1,14 @@
+// Выбор элементов в DOM-структуре
+const switchPseudoElement = document.getElementById("switch");
+const switchSlider = document.querySelector(".switcher__slider");
+
+// Дефолтный класс - псевдоэлемент переключателя
+switchPseudoElement.classList.add("switch-sun");
+
+// Слушатель событий на щелчок по слайдеру - изменение псевдоэлемента
+switchSlider.addEventListener("click", () => {
+  if ("click") {
+    switchPseudoElement.classList.toggle("switch-moon");
+    switchPseudoElement.classList.toggle("switch-sun");
+  }
+});
